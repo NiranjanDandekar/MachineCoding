524b451068bc5b3eebca9fc49bd8692705ace744
vivek anand <andvivek@amazon.com>
2024-12-05 10:52:46 +0530

    added necessary files for this template

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true, "This test should always pass");
+    }
+
+    // Add more tests as needed
+}
